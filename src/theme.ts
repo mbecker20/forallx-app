2665e4dbad2b5b8df09ef5a0006245f8ac23cad6
Max Becker <becker.maxh@gmail.com>
2020-06-09 22:03:12 -0700

    added colors to be used app wide

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+const grey = '#282c34' // grey
+const secondary = '#281c34' //purple
+const tertiary = '#182c54' // blue
+
+export interface Colors {
+  grey: string,
+  secondary: string,
+  tertiary: string,
+}
+
+const colors: Colors = {
+  grey: grey,
+  secondary: secondary,
+  tertiary: tertiary,
+}
+
+export default colors
+
+
