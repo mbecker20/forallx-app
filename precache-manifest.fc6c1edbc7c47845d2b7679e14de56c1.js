0f3c86496f22953177e123be50e2b8f6ce7291e5
Max Becker <becker.maxh@gmail.com>
2020-06-10 22:04:23 -0700

    Updates

---
+++ precache-manifest.fc6c1edbc7c47845d2b7679e14de56c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc52f37dc4674788cd1fea7b0d5fe03f",
+    "url": "/forallx-app/index.html"
+  },
+  {
+    "revision": "e3a15973a4632a0b36e3",
+    "url": "/forallx-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a783ba1e65fabe03abc5",
+    "url": "/forallx-app/static/js/2.6c5c0371.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/forallx-app/static/js/2.6c5c0371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a15973a4632a0b36e3",
+    "url": "/forallx-app/static/js/main.b2ef122b.chunk.js"
+  },
+  {
+    "revision": "f7b4111fbfccd12f75fb",
+    "url": "/forallx-app/static/js/runtime-main.9dac59e1.js"
+  }
+]);
